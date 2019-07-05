78ed61ccf311954e071d8959ee9e8ec10cb623fc
lapiscine <beleduedwin@yahoo.fr>
2019-07-05 10:44:45 +0200

    films et acteurs

---
+++ src/Movies.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Movies{
+
+    public static void main(String[] args) {
+
+
+        String[] title = {"INDIANA JONES ET LE ROYAUME DU CRÂNE DE CRISTAL",
+                "INDIANA JONES ET LA DERNIÈRE CROISADE",
+                "INDIANA JONES ET LE TEMPLE MAUDIT"};
+
+        String[][] actors = {
+
+                {"actor1", "actor2", "actor3"},
+                {"actor1", "actor2", "actor3"},
+                {"actor1", "actor2", "actor3"}
+        };
+
+        for(int i = 0; i < title.length; i++ ){
+
+            System.out.print("Dans le film " + title[i] + "Les principaux acteurs sont : " );
+            System.out.println(Arrays.toString(actors[i]));
+
+            //System.out.println(Arrays.toString(weapons[2]));
+
+
+        }
+
+
+    };
+
+}
